b6e00200d3544ad455a47c087137839f87925a5d
mili-emzz <m37009146@gmail.com>
2025-12-04 22:08:37 -0600

    tap view

--- views/GalleryView.kt
+++ views/GalleryView.kt
@@ -1,4 +1,84 @@
 package com.emiliagomez.intents_camera_app.views
 
-class GalleryView {
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import coil3.compose.rememberAsyncImagePainter
+import com.emiliagomez.intents_camera_app.R
+import com.emiliagomez.intents_camera_app.viewmodels.ScannerViewModel
+
+@Composable
+fun GalleryView(viewModel: ScannerViewModel) {
+    val context = LocalContext.current
+    val clipboard = LocalClipboardManager.current
+    var image: Any? by remember { mutableStateOf(R.drawable.gallery) }
+    val photoPicker = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia()
+    ) {
+        if (it != null) {
+            image = it
+            viewModel.onRecognizedTex(image, context)
+        } else {
+            viewModel.showToast(context, "Nothing picture was selected")
+        }
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            modifier = Modifier
+                .clickable {
+                    photoPicker.launch(
+                        PickVisualMediaRequest(
+                            ActivityResultContracts.PickVisualMedia.ImageOnly
+                        )
+                    )
+                }
+                .padding(16.dp, 8.dp),
+            painter = rememberAsyncImagePainter(
+                image
+            ),
+            contentDescription = null
+        )
+        Spacer(
+            modifier = Modifier.height(25.dp)
+        )
+        val scrollState = rememberScrollState()
+        Text(
+            viewModel.recognizedText,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .verticalScroll(scrollState)
+                .clickable{
+                    clipboard.setText(AnnotatedString(viewModel.recognizedText))
+                    viewModel.showToast(context, "Copied")
+                }
+        )
+    }
 }
--- views/TabsView.kt
+++ views/TabsView.kt
@@ -1,4 +1,62 @@
 package com.emiliagomez.intents_camera_app.views
 
-class TabsView {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.emiliagomez.intents_camera_app.viewmodels.ScannerViewModel
+
+/***
+ * Project: Recognition App
+ * Package: com.danielflores.recognitionapp.views
+ * Created by Kevin Daniel Flores Nataren
+ * File created at 04/abril/2025 at 16:16
+ * All rights reserved 2025.
+ **/
+
+@Composable
+fun TabsView(viewModel: ScannerViewModel){
+    var selectedTab by remember { mutableIntStateOf(0) }
+    val tabs = listOf("Gallery", "Camera", "Collection")
+    Scaffold {
+        Column(
+            modifier = Modifier.padding(it)
+        ) {
+            TabRow(
+                selectedTabIndex = selectedTab,
+                contentColor = Color.Black,
+                indicator = { position ->
+                    TabRowDefaults.SecondaryIndicator(
+                        Modifier.tabIndicatorOffset(position[selectedTab]),
+                        2.dp, Color.Black
+                    )
+                }
+            ) {
+                tabs.forEachIndexed { index, tab ->
+                    Tab(
+                        selected = selectedTab == index,
+                        onClick = { selectedTab = index },
+                        text = { Text(tabs[index]) }
+                    )
+                }
+            }
+            when(selectedTab){
+                0 -> GalleryView(viewModel).apply { viewModel.cleanText() }
+                1 -> CameraView(viewModel).apply { viewModel.cleanText() }
+                2 -> CollectionGalleryView()
+            }
+        }
+    }
 }
